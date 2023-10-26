678463d9f06d827caf08e428dd0cbae092d578c4
hhvc <hectorvazquez.laboral@gmail.com>
2023-10-25 23:42:45 -0300

    second commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,11 @@
 import Container from "react-bootstrap/Container"
+import CardItem from "./CardItem"
 
 export const ItemListContainer=(props)=>{
     return(
         <Container className="mt-4">
             <h1>{props.greeting}</h1>
+            <CardItem/>
         </Container>
     )
 }
