8a1a34782b003aa8ec96b77e3749698f3eb3c645
hhvc <hectorvazquez.laboral@gmail.com>
2024-01-03 00:20:50 -0300

    modifico vite.config.js para mejorar Chunks

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Configura manualmente cómo quieres agrupar tus módulos
-          lodash: ['lodash'],
-          react: ['react', 'react-dom'],
-        },
-      },
-    },
     chunkSizeWarningLimit: 1000, // Establece el límite de advertencia de tamaño del chunk
   },
 });
