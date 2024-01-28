ff06d318d0212e04ce4da3be834e53382ee3d620
hhvc <hectorvazquez.laboral@gmail.com>
2024-01-27 21:18:51 -0300

    agrego mostrar archivo .csv en componente MassVehicleUpload

--- src/components/MassVehicleUpload.jsx
+++ src/components/MassVehicleUpload.jsx
@@ -3,40 +3,27 @@ import {
   getFirestore,
   collection,
   getDoc,
-  getDocs,
   setDoc,
   doc,
   updateDoc,
+  getDocs,
 } from "firebase/firestore";
 import { saveAs } from "file-saver";
-// import { CsvWriter } from "filefy";
 import Papa from "papaparse";
 import { useAuth } from "../contexts/AuthContext";
 
-// Generador de ID automático
-function autoId() {
-  // Caracteres permitidos en el ID
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  // Longitud del ID
-  const autoIdLength = 20;
-
-  let autoId = "";
-  for (let i = 0; i < autoIdLength; i++) {
-    const randomChar = chars.charAt(Math.floor(Math.random() * chars.length));
-    autoId += randomChar;
-  }
-
-  return autoId;
-}
-
 const MassVehicleUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [csvData, setCsvData] = useState(null);
+  const [editedData, setEditedData] = useState(null);
+  const [showTable, setShowTable] = useState(false);
   const { userRole, user } = useAuth();
+  const [userNameComplete, setUserNameComplete] = useState("");
   const db = getFirestore();
 
-  // Verifica si el usuario tiene el rol necesario
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString();
+
   if (!["ADMINISTRADOR"].includes(userRole)) {
     return (
       <div className="container">
@@ -44,9 +31,6 @@ const MassVehicleUpload = () => {
       </div>
     );
   }
-
-  const currentDate = new Date();
-  const formattedDate = currentDate.toISOString();
 
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
@@ -60,14 +44,16 @@ const MassVehicleUpload = () => {
     }
 
     try {
-      let userName = "";
-      // Obtener el nombre y apellido del usuario desde Firestore
+
+      let userNameComplete ="";
+
       if (user) {
         const userDocRef = doc(db, "users", user.uid);
         const userDocSnapshot = await getDoc(userDocRef);
         if (userDocSnapshot.exists()) {
           const userData = userDocSnapshot.data();
-          userName = `${userData.nombre || ""} ${userData.apellido || ""}`;
+          userNameComplete = `${userData.nombre || ""} ${userData.apellido || ""}`;
+          setUserNameComplete(userNameComplete);
         }
       }
 
@@ -77,82 +63,95 @@ const MassVehicleUpload = () => {
       fileReader.onload = async (e) => {
         const csvData = e.target.result;
 
-        Papa.parse(csvData, {
-          complete: async (result) => {
-            console.log("Datos de vehículos:", result.data);
-
-            const automotoresCollection = collection(db, "automotores");
-
-            // Limpiar los datos eliminando propiedades no deseadas
-            const cleanData = result.data.map((vehicle) => {
-              const { __parsed_extra, ...cleanVehicle } = vehicle;
-              return cleanVehicle;
-            });
-
-            // Dentro del bucle for (const vehicle of result.data)
-            for (const vehicle of cleanData) {
-              try {
-                // Verificar si el vehículo tiene un ID
-                const docId = vehicle.id || autoId();
-                const docRef = doc(automotoresCollection, docId);
-                const docSnap = await getDoc(docRef);
-
-                // Eliminar las propiedades específicas que no deben ir en el objeto automotor
-                const {
-                  IMAGEN_DESTACADA,
-                  IMAGEN_FRENTE,
-                  IMAGEN_INTERIOR,
-                  IMAGEN_LATERAL,
-                  IMAGEN_TRASERA,
-                  ...vehicleWithoutImages
-                } = vehicle;
-
-                // Construye el objeto MAP, incluso si no hay datos
-                const imageMap = {
-                  DESTACADA: IMAGEN_DESTACADA || "",
-                  FRENTE: IMAGEN_FRENTE || "",
-                  INTERIOR: IMAGEN_INTERIOR || "",
-                  LATERAL: IMAGEN_LATERAL || "",
-                  TRASERA: IMAGEN_TRASERA || "",
-                };
-
-                // Crear un objeto con los datos del vehículo y el mapa de imágenes
-                const vehicleData = {
-                  ...vehicleWithoutImages,
-                  IMAGEN: imageMap,
-                  USUARIOCARGA: userName || "",
-                  FECHACARGA: formattedDate,
-                  FECHAMODIFICACION: formattedDate,
-                  ESTADOACTIVO: false,
-                };
-
-                if (docSnap.exists()) {
-                  // Actualiza solo los campos proporcionados
-                  await updateDoc(docRef, vehicleData);
-                  console.log(`Documento ${docId} actualizado con éxito.`);
-                } else {
-                  // Crea el documento en Firestore
-                  await setDoc(docRef, vehicleData);
-                  console.log(`Documento ${docId} insertado con éxito.`);
-                }
-              } catch (error) {
-                console.error(
-                  `Error al procesar el documento ${vehicle.id}:`,
-                  error
-                );
-              }
-            }
+        const parsedData = Papa.parse(csvData, { header: true }).data;
+        const enrichedData = parsedData.map((vehicle) => ({
+          ...vehicle,
+          IMAGEN: {
+            DESTACADA: vehicle.IMAGEN_DESTACADA || "",
+            FRENTE: vehicle.IMAGEN_FRENTE || "",
+            INTERIOR: vehicle.IMAGEN_INTERIOR || "",
+            LATERAL: vehicle.IMAGEN_LATERAL || "",
+            TRASERA: vehicle.IMAGEN_TRASERA || "",
           },
-          header: true,
-        });
+          USUARIOCARGA: userNameComplete || "",
+          FECHACARGA: formattedDate,
+          FECHAMODIFICACION: formattedDate,
+          ESTADOACTIVO: false,
+          id: vehicle.id || "", // Si no hay un id proporcionado, asigna un string vacío
+        }));
+
+        setCsvData(enrichedData);
+        setEditedData(enrichedData);
+        setShowTable(true);
       };
     } catch (error) {
       console.error("Error al procesar el archivo:", error);
     }
   };
 
-  const handleDownload = async () => {
+  const handleEdit = (index, columnName, value) => {
+    const editedDataCopy = [...editedData];
+    editedDataCopy[index][columnName] = value;
+    setEditedData(editedDataCopy);
+  };
 
+  const handleUploadToFirestore = async () => {
+    try {
+      const automotoresCollection = collection(db, "automotores");
+
+      for (const vehicle of editedData) {
+        const { __parsed_extra, id, ...restOfVehicle } = vehicle;
+
+        const docRef = id
+          ? doc(automotoresCollection, id)
+          : doc(automotoresCollection);
+        const docSnap = await getDoc(docRef);
+
+        const {
+          IMAGEN_DESTACADA,
+          IMAGEN_FRENTE,
+          IMAGEN_INTERIOR,
+          IMAGEN_LATERAL,
+          IMAGEN_TRASERA,
+          ...restOfImages
+        } = restOfVehicle;
+
+        const imageMap = {
+          DESTACADA: IMAGEN_DESTACADA || "",
+          FRENTE: IMAGEN_FRENTE || "",
+          INTERIOR: IMAGEN_INTERIOR || "",
+          LATERAL: IMAGEN_LATERAL || "",
+          TRASERA: IMAGEN_TRASERA || "",
+        };
+
+        const vehicleData = {
+          ...restOfImages,
+          IMAGEN: imageMap,
+          USUARIOCARGA: userNameComplete || "",
+          FECHACARGA: formattedDate,
+          FECHAMODIFICACION: formattedDate,
+          ESTADOACTIVO: false,
+        };
+
+        if (docSnap.exists()) {
+          await updateDoc(docRef, vehicleData);
+          console.log(`Documento ${docRef.id} actualizado con éxito.`);
+        } else {
+          await setDoc(docRef, vehicleData);
+          console.log(`Documento ${docRef.id} insertado con éxito.`);
+        }
+      }
+
+      setCsvData(null);
+      setEditedData(null);
+      setSelectedFile(null);
+      setShowTable(false);
+    } catch (error) {
+      console.error("Error al subir a Firestore:", error);
+    }
+  };
+
+  const handleDownload = async () => {
     const automotoresCollection = collection(db, "automotores");
 
     try {
@@ -162,7 +161,7 @@ const MassVehicleUpload = () => {
       snapshot.forEach((doc) => {
         const vehicleData = doc.data();
         const vehicleWithIdAndUrls = {
-          id: doc.id, // Agrega el ID del documento
+          id: doc.id,
           ...vehicleData,
           IMAGEN_DESTACADA: vehicleData.IMAGEN.DESTACADA,
           IMAGEN_FRENTE: vehicleData.IMAGEN.FRENTE,
@@ -173,13 +172,9 @@ const MassVehicleUpload = () => {
         vehicles.push(vehicleWithIdAndUrls);
       });
 
-      // Convierte los datos de los vehículos en un CSV
       const csv = Papa.unparse(vehicles);
-
-      // Crea un Blob con el contenido CSV
       const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
 
-      // Descarga el archivo CSV
       saveAs(blob, "automotores.csv");
     } catch (error) {
       console.error("Error al descargar la colección:", error);
@@ -202,8 +197,61 @@ const MassVehicleUpload = () => {
               onChange={handleFileUpload}
             />
             <button type="button" onClick={handleUpload}>
-              Subir Archivo
+              Mostrar datos
             </button>
+            {showTable && (
+              <div>
+                <h4>Datos del Archivo CSV</h4>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      {Object.keys(csvData[0]).map(
+                        (key) =>
+                          !["__parsed_extra", "IMAGEN"].includes(key) && (
+                            <th key={key}>{key}</th>
+                          )
+                      )}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {csvData.map((row, index) => (
+                      <tr key={index}>
+                        {Object.entries(row).map(
+                          ([key, value], colIndex) =>
+                            !["__parsed_extra", "IMAGEN"].includes(key) && (
+                              <td key={colIndex}>
+                                {typeof value === "object" ? (
+                                  <div>
+                                    {Object.entries(value).map(
+                                      ([imgKey, imgValue], imgIndex) => (
+                                        <div key={imgIndex}>
+                                          <strong>{imgKey}:</strong> {imgValue}
+                                        </div>
+                                      )
+                                    )}
+                                  </div>
+                                ) : (
+                                  <input
+                                    type="text"
+                                    value={editedData[index][key]}
+                                    onChange={(e) =>
+                                      handleEdit(index, key, e.target.value)
+                                    }
+                                  />
+                                )}
+                              </td>
+                            )
+                        )}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <button type="button" onClick={handleUploadToFirestore}>
+                  Subir a BD principal
+                </button>
+              </div>
+            )}
+
             <button type="button" onClick={handleDownload}>
               Descargar Colección
             </button>
